e46671f6414a870bd6f347d0dddd33086efc93f5
Pedro Barbosa <jpedro.barbosa@gmail.com>
2023-01-15 21:36:35 +0100

    fix

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ from repos import cli
 with open("README.md", "r") as f:
     readme = f.read()
 
-
 setup(
-    name="git-repos",
+    name="repos",
     version=cli.VERSION,
     description="Manages git repos inside a directory",
-    # long_description="Manages git repos inside a directory.",
     long_description=readme,
     long_description_content_type="text/markdown",
     readme="README.md",
@@ -25,17 +23,14 @@ setup(
         "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
     ],
-    # packages=find_packages(),
     packages=[
         "repos",
     ],
     install_requires=[
-        # "click",
         "pyyaml",
     ],
     entry_points={
         "console_scripts": [
-            "git-repos=repos.cli:main",
             "repos=repos.cli:main",
         ],
     },
